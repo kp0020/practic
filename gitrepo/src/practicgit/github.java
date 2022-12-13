0eacfc65da897bef35108455197999adce448aa2
Rushikesh <Rushikesh@Rushikesh>
2022-12-13 19:15:46 +0530

    1st attem

---
+++ gitrepo/src/practicgit/github.java
@@ -0,0 +1,22 @@
+package practicgit;
+
+public class github {
+
+	
+	public static void main(String[] args) {
+		
+	String str = "velocity is my class";
+	 String a[] = str.split(" ");
+		
+	//for (int i=str.length()-1; i>=0; i--) {
+			
+		
+		//System.out.print(str.charAt(i));
+		//str2 = str2+str.charAt(i);System.out.println(str2);
+	for (int i= a.length-1; i>=0;i--) {
+		System.out.println(a[i]);
+	}		
+	}
+	
+
+}
